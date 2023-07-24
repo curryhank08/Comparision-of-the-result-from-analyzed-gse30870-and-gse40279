d4c318c47239622a6840ea3135ec167e10af12fe
curryhank08 <curryhank08@gmail.com>
2023-07-25 00:42:25 +0900

    fixes & plots

--- Comparision_by_MEAL.R
+++ Comparision_by_MEAL.R
@@ -43,9 +43,38 @@ age_40279 <- as.numeric(age_40279)
 
 # Count the numbers of 90above and 30below (30 and 90 are included)
 gse40279_30below <- table(cut(age_40279, breaks = 0:30))
+gse40279_30below <- data.frame(gse40279_30below)
+colnames(gse40279_30below) = c("age", "Freq")
+
 gse40279_90above <- table(cut(age_40279, breaks = 89:110))
+gse40279_90above <- data.frame(gse40279_90above)
+
 gse40279_age30_90 <- data.frame(below30 = sum(gse40279_30below), up90 = sum(gse40279_90above), row.names = "numbers")
 
+age_values <- 1:30
+freq_values <- table(factor(age_40279, levels = age_values))
+gse40279_30below <- data.frame(freq_values)
+colnames(gse40279_30below) = c("age", "Freq")
+
+# Create the histogram plot
+ggplot(gse40279_30below, aes(x = age, y = Freq)) +
+  geom_col(fill = "skyblue", color = "black") +
+  labs(title = "Age Distribution (gse40279)", x = "Age", y = "Count")+
+  theme_minimal() +
+  theme(plot.title = element_text(hjust = 0.5))
+
+a <- data.frame(age_40279)
+ggplot(a, aes(x = age_40279)) + 
+  geom_histogram()
+
+# Create the histogram plot
+ggplot(a, aes(x = factor(age_40279)) +
+  geom_bar(fill = "skyblue", color = "black", stat = "count") +
+  scale_x_continuous(breaks = unique(a$age_40279)) +
+  labs(title = "Age Distribution", x = "Age", y = "Count") +
+  theme_minimal()
+
+'
 # Assign age values to a new column in pData of gse40279_matrix
 pData(gse40279_matrix)$age <- age_40279
 
@@ -70,25 +99,6 @@ res_40279 <- runPipeline(set = gse40279_matrix,
 # Extract the result of the DiffMean analysis
 result_40279 <- getProbeResults(res_40279, rid = 1, 
                                fNames = c("UCSC_RefGene_Name", "RANGE_START", "CHR", "ID"))
-'
-# Remove rows with missing values
-# result_Meal_clean <- na.omit(result_Meal)
-
-### Part 1-2
-'
-## manhattan plot for all
-library(qqman)
-# function from qqman to plot manhattan 
-result_Meal$CHR <- as.numeric(result_Meal$CHR)
-manhattan(result_Meal, 
-          main = "Manhattan Plot for gse40279 (Analysis of DiffMean on MEAL)",
-          cex = 0.6,
-          ylim = c(0, 25),
-          chr="CHR", 
-          bp="RANGE_START", 
-          snp= "ID", 
-          p="P.Value" )
-'
 
 # Extract samples belonging to "Young" and "Old" age categories
 gse40279_matrix_YO <- gse40279_matrix[, age_categories_40279 %in% c("Young", "Old")]
@@ -112,31 +122,6 @@ result_40279_YO <- getProbeResults(res_40279_YO, rid = 1,
 result_40279_YO_clean <- na.omit(result_40279_YO)
 
 
-'
-# Create a new age category with 2 levels (Middle and Old)
-MO_age_categories <- factor(age_categories, levels = c("Middle", "Old"))
-MO_age_categories <- na.omit(MO_age_categories)
-
-# Extract samples belonging to "Middle" and "Old" age categories
-subset_forMeal_MO <- gse40279_matrix[, age_categories %in% c("Middle", "Old")]
-subset_forMeal_MO_data <- exprs(subset_forMeal_MO)
-# Assign the new age category to pData of the subset
-pData(subset_forMeal_MO)$MO_age_category <- MO_age_categories
-
-# Run MEAL pipeline on the subset YO
-res_sub_MO <- runPipeline(set = subset_forMeal_MO,
-                          variable_names = "MO_age_category",
-                          betas = TRUE,
-                          analyses = c("DiffMean", "DiffVar"))
-
-# Extract the result of the DiffMean analysis
-result_Meal_sub_MO <- getProbeResults(res_sub_MO, rid = 1, 
-                                      fNames = c("UCSC_RefGene_Name", "RANGE_START", "CHR", "ID"))
-
-# Remove rows with missing values
-result_Meal_sub_MO_clean <- na.omit(result_Meal_sub_MO)
-'
-
 ### Part 2 (30870)
 
 # Create age categories
@@ -156,7 +141,7 @@ res_30870 <- runPipeline(set = gse30870_matrix,
 result_30870 <- getProbeResults(res_30870, rid = 1, 
                                fNames = c("UCSC_RefGene_Name", "RANGE_START", "CHR", "ID"))
 
-# Create a subset of gse30870_matrix to match gse40279_matrix_YO's probes
+# Create a subset of gse30870_matrix to match gse40279_matrix_YO s probes
 gse30870_matrix_sub <- gse30870_matrix[fData(gse30870_matrix)$ID %in% fData(gse40279_matrix_YO)$ID]
 
 # Run MEAL pipeline on the categorized subset data
@@ -170,205 +155,6 @@ result_30870_sub <- getProbeResults(res_30870_sub, rid = 1,
                                 fNames = c("UCSC_RefGene_Name", "RANGE_START", "CHR", "ID"))
 
 
-'
-### Part 1-2
-## manhattan plot for all
-library(qqman)
-# function from qqman to plot manhattan 
-result_Meal$CHR <- as.numeric(result_Meal$CHR)
-manhattan(result_Meal, 
-          main = "Manhattan Plot for gse40279 (Analysis of DiffMean on MEAL)",
-          cex = 0.6,
-          ylim = c(0, 25),
-          chr="CHR", 
-          bp="RANGE_START", 
-          snp= "ID", 
-          p="P.Value" )
-
-## Subset the data matrix for the filtered samples
-# filtered_data <- data[, colnames(data) %in% rownames(young_old_samples)]
-
-# Create a new age category with 2 levels (Old and Young)
-new_age_categories <- factor(age_categories, levels = c("Old", "Young"))
-new_age_categories <- na.omit(new_age_categories)
-
-# Extract samples belonging to "Young" and "Old" age categories
-subset_forMeal_YO <- gse40279_matrix[, age_categories %in% c("Young", "Old")]
-subset_forMeal_YO_data <- exprs(subset_forMeal_YO)
-# Assign the new age category to pData of the subset
-pData(subset_forMeal_YO)$new_age_category <- new_age_categories
-# subeset samples info
-subset_forMeal_YO_samplesinfo <- pData(subset_forMeal_YO)
-
-# Run MEAL pipeline on the subset YO
-res_sub_YO <- runPipeline(set = subset_forMeal_YO,
-                          variable_names = "new_age_category",
-                          betas = TRUE,
-                          analyses = c("DiffMean", "DiffVar"))
-
-# Extract the result of the DiffMean analysis
-result_Meal_sub_YO <- getProbeResults(res_sub_YO, rid = 1, 
-                                      fNames = c("UCSC_RefGene_Name", "RANGE_START", "CHR", "ID"))
-
-# Remove rows with missing values
-result_Meal_sub_YO_clean <- na.omit(result_Meal_sub_YO)
-
-
-## manhattan plot for subset YO 
-install.packages("qqman") # if you have not installed
-library(qqman)
-
-# Extract data for manhattan plot
-res_M_YO_manhattan <- result_Meal_sub_YO_clean
-
-# Convert CHR column to numeric
-res_M_YO_manhattan$CHR <- as.numeric(res_M_YO_manhattan$CHR)
-
-# Remove rows with missing values
-res_M_YO_manhattan_clean <- na.omit(res_M_YO_manhattan)
-
-# function from qqman to plot manhattan 
-manhattan(res_M_YO_manhattan_clean, 
-          main = "Manhattan Plot for gse40279 (subset_YO) (Analysis of DiffMean on MEAL) ",
-          cex = 0.3,
-          ylim = c(0, 50),
-          chr="CHR", 
-          bp="RANGE_START", 
-          snp= "ID", 
-          p="P.Value",
-          genomewideline = FALSE,
-          suggestiveline = -log10(1e-05))
-
-## volcano plot
-# Extract data for volcano plot (same as res_manhattan)
-res_M_volcano <- result_Meal 
-# Remove rows with missing values
-res_M_volcano_clean <- na.omit(res_M_volcano)
-
-# Add log-transformed p-value column to res_M_volcano_clean
-res_M_volcano_clean$neg_logP <- -log10(res_M_volcano_clean$P.Value)
-
-# Create volcano plot
-ggplot(res_M_volcano_clean, aes(x = logFC, y = neg_logP)) +
-  geom_point(size = 0.5, color = "BLUE")+
-  ggtitle("Volcano plot for gse40279 (Analysis of DiffMean on MEAL)")+
-  labs(x="log2(Flod change)", y="-log10(p-value)")
-
-# Way 2 to plot volcano 
-plot(res, rid = "DiffMean", type = "volcano", tPV = 14, tFC = 0.1, 
-     show.labels = FALSE) + ggtitle("Volcano plot for gse40279 (Analysis of DiffMean on MEAL)")
-
-# Way 2 to plot volcano for subset YO
-plot(res_sub_YO, rid = "DiffMean", type = "volcano", tPV = 14, tFC = 0.1, 
-     show.labels = FALSE) + ggtitle("Volcano plot for gse40279 (subset YO)(Analysis of DiffMean on MEAL)")
-
-# QQ plot for all
-plot(res, rid = 1, type = "qq") + ggtitle("QQplot for gse40279 (Analysis of DiffMean on MEAL)")
-
-# QQ plot for subset YO
-plot(res_sub_YO, rid = 1, type = "qq") 
-+ ggtitle("QQplot for gse40279 (subset YO)(Analysis of DiffMean on MEAL)")
-
-# QQ plot for subset MO
-plot(res_sub_MO, rid = 1, type = "qq") + ggtitle("QQplot for gse40279 (subset MO)(Analysis of DiffMean on MEAL)")
-
-# Plot the beta values distribution of a CpG
-plotFeature(set = gse40279_matrix, feat = "cg16762684", variables = "age_category") + 
-  ggtitle("cg16762684 (Lowest P.value on the analysis of DIffMean)") +
-  ylab("Methylation (Beta value)")
-
-
-
-
-## (Error) not yet solved
-# Regional plotting 
-targetRange <- GRanges("18:60000000-100000000")
-
-# Check the current value of fData(res)$Strand
-currentStrand <- fData(gse40279_matrix)$Strand
-
-# Transform "+" if the value is "F", and "-" if the value is "R" 
-transformedStrand <- ifelse(currentStrand == "F", "+", ifelse(currentStrand == "R", "-", currentStrand))
-
-# Assign the transformed values back to fData(res)$Strand
-fData(gse40279_matrix)$Strand <- transformedStrand
-
-# Run MEAL pipeline on the categorized data
-res <- runPipeline(set = gse40279_matrix,
-                   variable_names = "age_category",
-                   betas = TRUE,
-                   analyses = c("DiffMean", "DiffVar"))
-
-plotRegion(res, targetRange)
-plotRegion(res, targetRange, results = c("DiffMean"), tPV = 24, fNames = c("chromosome", "start", "end"))
-## (Error) not yet solved
-'
-
-'
-### Part 2
-## Load gse40279 and run analysis by using "limma" packages (from original code limma.R)
-library(limma)
-# Load modified GEOquery
-library(GEOquery)
-# Setting the max timeout_seconds
-options(timeout=100000)
-# Check the input timeout_seconds
-getOption("timeout")
-# Download GSE40279 by a fuction getGEO() from modified GEOquery package.
-gse40279 <- getGEO("GSE40279", GSEMatrix = TRUE, AnnotGPL = TRUE) # if have not downloaded
-
-gset <- gse40279
-if (length(gset) > 1) idx <- grep("GPL13534", attr(gset, "names")) else idx <- 1
-gset <- gset[[idx]]
-
-# Create age categories
-age <- pData(gset)$characteristics_ch1
-# Remove "age (y):" and convert to numeric
-age <- sub("^\\s*age \\(y\\): ", "", age)
-age <- as.numeric(age)
-# Assign age values to a new column in pData of gset
-pData(gset)$age <- age
-
-# Define age categories based on specific age ranges
-age_categories <- cut(age,
-                      breaks = c(0, 30, 65, Inf),
-                      labels = c("Young", "Middle", "Old"),
-                      include.lowest = TRUE)
-
-# Assign age categories to the pData of gset
-pData(gset)$age_category <- age_categories
-
-ex <- exprs(gset)
-# log2 transform
-qx <- as.numeric(quantile(ex, c(0., 0.25, 0.5, 0.75, 0.99, 1.0), na.rm=T))
-LogC <- (qx[5] > 100) ||
-  (qx[6]-qx[1] > 50 && qx[2] > 0)
-if (LogC) { ex[which(ex <= 0)] <- NaN
-ex <- log2(ex)}
-
-conditions <- gset$age_category
-f <- factor(conditions, levels = c("Young", "Middle", "Old"))
-design <- model.matrix(~0+f)
-colnames(design) <- c("Young", "Middle", "Old")
-fit <- lmFit(gset, design)
-contrast.matrix <- makeContrasts(Young-Middle, Young-Old, Middle-Old, levels=design)
-fit2 <- contrasts.fit(fit, contrast.matrix)
-fit2 <- eBayes(fit2)
-
-result_limma_YM <- topTable(fit2, coef=1, number = Inf, adjust.method = "BH", sort.by = "logFC")
-result_limma_YO <- topTable(fit2, coef=2, number = Inf, adjust.method = "BH", sort.by = "logFC")
-result_limma_MO <- topTable(fit2, coef=3, number = Inf, adjust.method = "BH", sort.by = "logFC")
-
-# Outcome of each hypothesis test
-results <- decideTests(fit2)
-
-# Showing numbers of genes significant in each comparison
-vennDiagram(results)
-
-# Remove rows with missing values
-# result_limma_coef1_clean <- na.omit(result_limma_2)
-'
-
 ### Part 3
 ## Merge the results of analysis from gse30870 and gse40279, and then create a scatter plot.
 library(ggplot2)
@@ -381,12 +167,6 @@ merged_30870_40279_p <- merge(res_30870_YO_p,
                           res_40279_YO_p,
                          by.x = "ProbeID",
                          by.y = "ProbeID")
-'
-ggplot(merged_30870_40279_p, aes(x = log(p_30870), y = log(p_40279)) +
-  geom_point(alpha = 0.7, color = black, size = 0.5) +
-  labs(x = "p-value in GSE30870", y = "p-value in GSE40279") +
-  theme_minimal()
-'
 
 plot(log(merged_30870_40279_p$p_30870), log(merged_30870_40279_p$p_40279), 
        xlab = "P.value from analysis of gse30870", ylab = "P.value from analysis of gse40279", 
@@ -444,16 +224,4 @@ for (x in res_30870_YO_p$probe_name_30870) {
     different_probe <- rbind(different_probe, row_to_add)
   }
 }
-'
-
-'
-# Mean-Difference Plot from limma
-as.numeric(limma_Meal_YO_p$limma_p)
-as.numeric(limma_Meal_YO_p$MEAL_p)
-x1 <- log(limma_Meal_YO_p$limma_p)
-x2 <- log(limma_Meal_YO_p$MEAL_p)
-oldpar <- par(mfrow=c(1,2))
-plot(x1,x2,xlab="loge(limma_p)",ylab="loge(MEAL_p)",main="Scatter Plot")
-mdplot(cbind(x1,x2),bg.pch=1,bg.cex=1,main = "Mean-Difference Plot")
-par(oldpar)
 '
