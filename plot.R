ec18b99c0d68241b28fe0400cb19d4719995679c
curryhank08 <yaohank@yaobohandeMacBook-Air.local>
2023-08-27 21:43:43 +0800

    fix & plots

--- plot.R
+++ plot.R
@@ -43,11 +43,15 @@ manhattan(result_limma_x1x2,
 library(ggplot2)
 ggplot(result_30870_sub, aes(x = P.Value)) +
   geom_histogram(binwidth = 0.02) +
+  ggtitle("Histogram plot for P.Value (gse30870)")
+
+ggplot(result_30870_sub, aes(x = P.Value)) +
+  geom_histogram() +
   ggtitle("Distribution of P.Value (gse30870)") +
   scale_y_continuous(
-    breaks = seq(0, 125000, by = 25000),     # Specify the desired breaks for y-axis
-    labels = seq(0, 125000, by = 25000),     # Specify the corresponding labels
-    limits = c(0, 125000),
+    breaks = seq(0, 50000, by = 10000),     # Specify the desired breaks for y-axis
+    labels = seq(0, 50000, by = 10000),     # Specify the corresponding labels
+    limits = c(0, 50000),
     expand = c(0, 0)                  # Adjust the expand parameter for y-axis
   ) +
   scale_x_continuous(
@@ -57,4 +61,28 @@ ggplot(result_30870_sub, aes(x = P.Value)) +
     expand = c(0, 0)      # Adjust the expand parameter to remove extra space
   ) 
 
+ggplot(result_30870_sub, aes(x = P.Value)) +
+  geom_histogram() +
+  ggtitle("Distribution of P.Value (gse30870)") +
+  scale_x_continuous(
+    breaks = seq(0, 1, by = 0.1),
+    labels = seq(0, 1, by = 0.1),
+    limits = c(0, 1),  # Set limits to match xlim
+    expand = c(0, 0)      # Adjust the expand parameter to remove extra space
+  ) 
 
+# Create a histogram using base R plotting functions
+hist(result_30870_sub$P.Value, 
+     breaks = seq(0, 1, by = 0.01),  # Specify the bin breaks
+     col = "skyblue",                # Set color of bars
+     main = "Histogram plot for gse30870-P.Value",  # Set main title
+     xlab = "P.Value",               # Label for x-axis
+     ylab = "Count")             # Label for y-axis
+
+# Create a histogram using base R plotting functions
+hist(result_30870_sub$P.Value, 
+     breaks = seq(0, 1, by = 0.01),  # Specify the bin breaks
+     col = "skyblue",                # Set color of bars
+     main = "Histogram plot for gse30870-P.Value",  # Set main title
+     xlab = "P.Value",               # Label for x-axis
+     ylab = "Count")             # Label for y-axis
