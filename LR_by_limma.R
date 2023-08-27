0e44d0f822664a7716e1f3d6318f32806ae81b2f
curryhank08 <yaohank@yaobohandeMacBook-Air.local>
2023-08-28 00:45:18 +0800

    fixes & plots

--- LR_by_limma.R
+++ LR_by_limma.R
@@ -48,10 +48,67 @@ result_limma_x1 <- topTable(fit, coef="x1", number = Inf, adjust.method = "BH", 
 result_limma_x1x2 <- topTable(fit, coef="x1:x2", number = Inf, adjust.method = "BH", sort.by = "P")
 
 # result_limma_2 <- result_limma[, c("UCSC_RefGene_Name", "P.Value", "logFC")]
-
-
 # Outcome of each hypothesis test
 results <- decideTests(fit, method = "separate", adjust.method = "BH", p.value = 1e-5)
 
 # Showing numbers of genes significant in each comparison
 vennDiagram(results, show.include = FALSE) + title('Numbers of genes significant (p.value<1e-5) in each comparison')
+
+# Install ggVennDiagram to plot venndiagram
+install.packages("ggVennDiagram") # Optional
+
+library(ggVennDiagram)
+# Assuming you have the adjusted p-values for each probe in each dataset
+adjusted_p_values_gse30870sub <- result_30870_sub$adj.P.Val
+adjusted_p_values_gse40279_x1 <- result_limma_x1$adj.P.Val
+adjusted_p_values_gse40279_x1x2 <- result_limma_x1x2$adj.P.Val
+
+# Specify the condition
+condition_p <- 0.05
+
+# Create logical vectors indicating if the adjusted p-values meet the condition
+logical_vector_gse30870 <- as.integer(adjusted_p_values_gse30870sub < condition_p)
+logical_vector_gse40279_x1 <- as.integer(adjusted_p_values_gse40279_x1 < condition_p)
+logical_vector_gse40279_x1x2 <- as.integer(adjusted_p_values_gse40279_x1x2 < condition_p)
+
+# data frame of the values for the probes' adj.pvalue under condition
+data <- data.frame(
+  `gse30870` = logical_vector_gse30870,
+  `gse40279_x1` = logical_vector_gse40279_x1,
+  `gse40279_x1x2` = logical_vector_gse40279_x1x2
+)
+row.names(data) <- probe_ids
+
+# list of the values for the probes' adj.pvalue under condition
+data_for_venn <- list(gse30870_adjp = logical_vector_gse30870,
+                      gse40279_adjp_x1 = logical_vector_gse40279_x1,
+                      gse40279_adjp_x1x2 = logical_vector_gse40279_x1x2)
+
+ggVennDiagram(data_for_venn)
+
+# Load the necessary library
+library(VennDiagram)
+library(ggplot2)
+
+# Create your gene sets based on the condition (adj.pvalue < 0.01(Optional))
+set1 <- (result_30870_sub[result_30870_sub$adj.P.Val < 1e-8,])$ID
+set2 <- (result_limma_x1[result_limma_x1$adj.P.Val < 1e-8,])$ID
+set3 <- (result_limma_x1x2[result_limma_x1x2$adj.P.Val < 1e-8,])$ID
+
+# Create a list of sets
+sets_list <- list('30870' = set1, '40279-x1' = set2, '40279-x1x2' = set3)
+
+# Plot the Venn diagram
+venn1 <- ggVennDiagram(sets_list)
+venn1 + 
+  labs(title = "Venn Diagram",
+       subtitle = "Note: adj.P.value < 1e-8",
+       caption = paste(Sys.Date(), "Yao")) +
+  scale_x_continuous(expand = expansion(mult = .2)) +
+  theme(
+  plot.title = element_text(hjust = 0.1, vjust = 10, size = 20, face = "bold"),  # Adjust title positioning
+  plot.subtitle = element_text(hjust = 0.1, vjust = 15),  # Adjust subtitle positioning
+  plot.margin = margin(t = 0, r = 10, b = 0, l = 0, unit = "pt")  # Adjust the bottom margin
+)
+
+theme(plot.title = element_text(hjust = 0.5, size = 20, face = "bold"))
